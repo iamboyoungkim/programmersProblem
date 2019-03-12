fa8bbcc5961167da70c6283c04a68401f1bf83ac
iamboyoungkim <boyoung3205@gmail.com>
2019-03-12 10:21:16 +0900

    solve 2 problems

---
+++ k번째수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(auto x: commands){
+        int start = x[0];
+        int end = x[1];
+        int pick = x[2];
+        
+        vector<int> tmp;
+        for(int i = start-1; i<end; i++){
+            tmp.push_back(array[i]);
+        }
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp[pick-1]);
+    }
+    return answer;
+}
