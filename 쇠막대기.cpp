72956b43fbfecc9de9b1710ab9c8e645fa9c71b5
iamboyoungkim <boyoung3205@gmail.com>
2019-07-10 17:40:11 +0900

    solve 1 problem

---
+++ 쇠막대기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    stack<char> st;
+    
+    for (int i = 0; i<arrangement.length(); i++){
+        if (arrangement[i] == '(') st.push(arrangement[i]);
+        else {
+            st.pop();
+            if (arrangement[i-1] == '(') answer += st.size();
+            else answer++;
+        }
+    }
+    return answer;
+}
