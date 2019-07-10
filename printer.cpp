94cc75cde5d287b843cc240c5a8d68eba253ce3d
iamboyoungkim <boyoung3205@gmail.com>
2019-07-10 10:53:38 +0900

    solve 1 problem

---
+++ printer.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
+    int size = priorities.size();
+    for (int i = 0; i < size; i++) {
+        q.push(make_pair(i, priorities[i])); // 순서, 중요도
+        pq.push(priorities[i]);
+    }
+    
+    while(!q.empty()) {
+        // 중요도로 정렬한 순서랑 같으면
+        if (q.front().second == pq.top()) {
+            // location이 같으면 (정답인 경우)
+            if (q.front().first == location) {
+                return answer+1;
+            }
+            else {
+                q.pop();
+                pq.pop();
+                answer++;
+            }
+        }
+        else {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    return answer;
+}
