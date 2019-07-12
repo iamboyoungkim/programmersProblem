55859f2da7f0d2d121518d692ea793dcfe196cb5
iamboyoungkim <boyoung3205@gmail.com>
2019-07-12 16:10:15 +0900

    refactoring some problems

--- 가장큰수.cpp
+++ 가장큰수.cpp
@@ -4,23 +4,22 @@
 
 using namespace std;
 
-bool comp(const string &a, const string &b){
-    if (b+a < a+b) return true;
+bool compare(const string &a, const string &b) {
+    if (a+b > b+a) return true;
     else return false;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
-    int zeroCnt = 0;
     vector<string> tmp;
+    int zero = 0;
     
-    for (int i = 0; i<numbers.size(); i++) {
-        if (numbers[i] == 0) zeroCnt++;
+    for (int i = 0; i<numbers.size(); i++){
+        if (numbers[i] == 0) zero++;
         tmp.push_back(to_string(numbers[i]));
     }
-    
-    if (zeroCnt == numbers.size()) return "0";
-    sort(tmp.begin(), tmp.end(), comp);
+    if (zero == numbers.size()) return "0";
+    sort(tmp.begin(), tmp.end(), compare);
     
     for (int i = 0; i<tmp.size(); i++){
         answer += tmp[i];
--- 소수찾기.cpp
+++ 소수찾기.cpp
@@ -3,47 +3,35 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <set>
 
 using namespace std;
 
-int isPrime[100000000];
+bool isPrime[100000000];
 
-void eratoesthenes() {
+// 에라토스테네스의 체 방식 사용
+void findPrimes() {
     for (int i = 2; i<=9999999; i++){
-        isPrime[i] = 1;
+        isPrime[i] = true;
     }
     for (int i = 2; i<=sqrt(9999999); i++){
         if (isPrime[i]) {
             for (int j = i+i; j<=9999999; j+=i){
-                isPrime[j] = 0;
+                isPrime[j] = false;
             }
         }
     }
 }
 
 int solution(string numbers){
-    eratoesthenes();
-    int answer = 0;
-    int size = numbers.size();
-    vector<int> v(size);
-    for (int i = 0; i<size; i++){
-        v[i] = 1;
-        sort(numbers.begin(), numbers.end());
-        do {
-            int tmp = 0;
-            int j = 1;
-            for (int k = 0; k<size; k++){
-                if (v[k] == 1){
-                    tmp += (numbers[k] - '0') * j;
-                    j *= 10;
-                }
-            }
-            if (isPrime[tmp] == 1){
-                cout << tmp << '\n';
-                isPrime[tmp] = 2;
-                answer++;
-            }
-        } while(next_permutation(numbers.begin(), numbers.end()));
-    }
-    return answer;
+    findPrimes();
+    sort(numbers.begin(), numbers.end());
+    set<int> primes;
+    do {
+        for (int i = 1; i<=numbers.size(); i++) {
+            string sub = numbers.substr(0, i);
+            if (isPrime[stoi(sub)]) primes.insert(stoi(sub));
+        }
+    } while(next_permutation(numbers.begin(), numbers.end()));
+    return primes.size();
 }
