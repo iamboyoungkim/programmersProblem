01a836cc0d31c050acbe7b8d1f5be1d6fd07cb30
iamboyoungkim <boyoung3205@gmail.com>
2019-08-02 19:44:47 +0900

    solve problems

---
+++ 라면공장.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>> pq;
+
+int solution(int stock, vector<int> dates, vector<int> supplies, int k) {
+    int answer = 0;
+    int day = 0;
+    
+    for (int i = 0; i<k; i++) {
+        if (i == dates[day]) {
+            pq.push(supplies[day]);
+            if (day != supplies.size() - 1) day++;
+        }
+        if (stock == 0) {
+            answer++;
+            stock += pq.top();
+            pq.pop();
+        }
+        stock--;
+    }
+    return answer;
+}
