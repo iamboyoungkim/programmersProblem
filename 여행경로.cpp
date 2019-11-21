98cc8a81d83bd760e863a2b4118f7fadd0262c2f
iamboyoungkim <boyoung3205@gmail.com>
2019-11-21 15:28:58 +0900

    solve problems

--- 여행경로.cpp
+++ 여행경로.cpp
@@ -1,25 +1,43 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-bool visit[10001];
 vector<string> answer;
+vector<string> tmp;
 
-void dfs(string start, vector<vector<string>> tickets) {
-    answer.push_back(start);
-    for (int i = 0; i<tickets.size(); i++){
-        if (tickets[i][0] == start && visit[i] == false) {
-            visit[i] = true;
-            dfs(tickets[i][1], tickets);
-            return;
+bool dfs(string start, vector<vector<string>> & tickets, vector<bool> &isVisit, int idx) {
+
+    tmp.push_back(start);
+    
+    // 정답인 경우
+    if (idx == tickets.size()) {
+        answer = tmp;
+        return true;
+    }
+    
+    // 다음 단계 탐색
+    for (int i = 0; i<tickets.size(); i++) {
+        if (tickets[i][0] == start && !isVisit[i]) {
+            isVisit[i] = true;
+            if (dfs(tickets[i][1], tickets, isVisit, idx+1)) return true;
+            isVisit[i] = false;
         }
     }
+    tmp.pop_back();
+    return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
+    vector<bool> isVisit(tickets.size(), false);
+    
+    // 알파벳 순서대로 정렬
     sort(tickets.begin(), tickets.end());
-    dfs("ICN", tickets);
+    
+    // 탐색 시작
+    dfs("ICN", tickets, isVisit, 0);
+    
     return answer;
 }
--- 입국심사.cpp
+++ 입국심사.cpp
@@ -1,21 +1,18 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+// 문제가 개편 되었습니다. 이로 인해 함수 구성이 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+#include <string>
 #include <vector>
-#include <queue>
-#include <stack>
 
 using namespace std;
 
-int solution(int n, vector<int> times) {
+long long solution(int n, vector<int> times) {
     long long left = 1;
     long long right = 100000000000000;
-
+    
     while (left <= right) {
         long long sum = 0;
         long long mid = (left + right) / 2;
-        for (int i = 0; i < times.size(); i++) {
+        for (int i = 0; i<times.size(); i++) {
             sum += mid / times[i];
         }
         if (sum >= n) right = mid - 1;
